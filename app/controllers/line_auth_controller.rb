b380c1865beb8c0f5fe595808a602751456ec49a
SK-Study640 <m9bp67nkma@gmail.com>
2025-01-05 20:28:12 +0900

    プロフィール画像をローカルに保存する機能をせいぞうちゅう

--- app/controllers/line_auth_controller.rb
+++ app/controllers/line_auth_controller.rb
@@ -3,30 +3,46 @@ class LineAuthController < ApplicationController
 
   def callback
     auth = request.env["omniauth.auth"]  # LINEからの認証情報を取得
-    user = User.find_or_initialize_by(uid: auth["uid"], provider: auth["provider"])
-    user.name = auth["info"]["name"]
-    user.line_user_id = auth["uid"]
-    user.provider = auth["provider"]
-    # LINEから画像URLを取得してローカルに保存
-    if auth["info"]["image"].present?
-      begin
-        image_file = URI.open(auth["info"]["image"]) # LINEの画像URLから取得
-        user.profile_image.attach(io: image_file, filename: "profile_#{user.id}.jpg", content_type: "image/jpeg")
-      rescue => e
-        Rails.logger.error "プロフィール画像取得エラー: #{e.message}"
-      end
-    else
-      # LINEから画像URLが取得できない場合はデフォルト画像を設定
-      user.profile_image.attach(io: File.open(Rails.root.join("app/assets/images/default_profile.png")), filename: "default.jpg", content_type: "image/jpeg")
-    end
-    user.save!
+    @user = User.find_or_initialize_by(uid: auth["uid"], provider: auth["provider"])
+    @user.name = auth["info"]["name"]
+    @user.line_user_id = auth["uid"]
+    @user.provider = auth["provider"]
+    image_file_path = image(auth, @user.id)
     # ユーザーをログイン状態にする
-    session[:user_id] = user.id
-    redirect_to new_registrations_path
+    session[:@user_id] = @user.id
+    redirect_to new_registrations_path(user_id: @user.id, user_name: @user.name, user_uid: @user.line_user_id, user_provider: @user.provider, image_file_path: image_file_path)
   end
 
   def failure
     Rails.logger.error "LINE認証エラー: #{e.message}"
     redirect_to root_path, alert: "LINE認証に失敗しました"
   end
+
+  def image(auth, user_id)
+    # LINEから画像URLを取得して一時ファイルとして保存
+    if auth["info"]["image"].present?
+      begin
+        image_file = URI.open(auth["info"]["image"]) # LINEの画像URLから取得
+      rescue => e
+        Rails.logger.error "プロフィール画像取得エラー: #{e.message}"
+      end
+    else
+        # LINEから画像URLが取得できない場合はデフォルト画像を設定
+        image_file = File.open(Rails.root.join("app/assets/images/default_profile.png"))
+    end
+    file_path = Rails.root.join("tmp", user_id.to_s)
+    File.open(file_path, "wb") { |f| f.write(image_file) }
+    file_path
+  end
+
+  # def process
+  #   temp_image_path = params[:temp_image_path]
+  #   if temp_image_path && File.exist?(temp_image_path)
+  #     # 画像を処理
+  #     File.delete(temp_image_path) # 処理後に一時ファイルを削除
+  #   else
+  #     flash[:alert] = "画像ファイルが見つかりません。"
+  #     redirect_to new_upload_path
+  #   end
+  # end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,12 @@
 class RegistrationsController < ApplicationController
-  def new
+  before_action :set_user
+
+  def new(user)
     # 新規登録画面表示
     @user = User.find(session[:user_id])
   end
 
-  def confirm
+  def confirm(user)
     # 入力内容確認画面表示
     @user = User.find(session[:user_id])
     @year = params[:year]
@@ -14,5 +16,19 @@ class RegistrationsController < ApplicationController
 
   def complete
     # 登録完了画面表示
+    #
+    # Active record登録用コード備忘
+    # @user.profile_image.attach(io: image_file, filename: "profile_#{@user.id}.jpg", content_type: "image/jpeg")
+    #
+  end
+
+  private
+  def set_user
+    @user = User.new(
+      params[:user_id],
+      params[:user_name],
+      params[:user_uid],
+      params[:user_provider]
+    )
   end
 end
